4022aac6310d297e1717991f480d331ddb77cc9c
Ben Herman <bherman24@dtechhs.org>
2022-10-24 22:09:29 -0700

    auto
    added auto code and the requirements to run it

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,44 +1,49 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.GutPrototype;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  private Command m_autonomousCommand;
+  private RobotContainer m_robotContainer;
 
   public static XboxController controller = new XboxController(0);
 
   GutPrototype gutPrototype = new GutPrototype();
 
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    m_robotContainer = new RobotContainer();
+  }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
   @Override
-  public void autonomousInit() {}
+  public void autonomousInit() {
+    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
 
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.schedule();
+    }
+  }
+    
   @Override
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {}
-
+  public void teleopInit() {
+    if (m_autonomousCommand != null) {
+      m_autonomousCommand.cancel();
+    }
+  }
+    
   @Override
   public void teleopPeriodic() {
     configureTeleopControls();
