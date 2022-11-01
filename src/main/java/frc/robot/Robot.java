70992f4a16dc6e7b7970e5588f36dd4f1d6eb78b
Ben Herman <bherman24@dtechhs.org>
2022-11-01 12:33:38 -0700

    fixed I bit of stuff

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -2,13 +2,10 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.GutPrototype;
 
 
 public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
   private RobotContainer m_robotContainer;
 
   public static XboxController controller = new XboxController(0);
