526f816b8c22ce7fb820ba72ccdb553f6747efd8
Ben Herman <bherman24@dtechhs.org>
2022-10-31 17:52:45 -0700

    needs testing

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,19 +1,25 @@
 package frc.robot.subsystems;
+import java.security.Principal;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMax.IdleMode;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.XboxController;
+
+
 public class Shooter {
       
   // declare motor and encoder
   private CANSparkMax motor1 = new CANSparkMax(1, MotorType.kBrushless);
   private RelativeEncoder encoder = motor1.getEncoder();
-
+  private PIDController PIDController = new PIDController(1, 0, 0);
   private double setpoint = 0.0;
   private ShooterState systemState = ShooterState.IDLE;
   private boolean requestShoot = false;
-  private int timeLastStateChange;
+  private double timeLastStateChange;
   public Shooter() {
 
   }
@@ -27,11 +33,17 @@ public class Shooter {
   }
 
   public void setFlywheelRPM(double rpm) {
-    if (rpm == 0.0) {
+    double e = rpm - getVelocity();
+    if (rpm <= 0.1) {
       motor1.set(0.0);
     } else {
-      motor1.set(rpm);;
+      motor1.set(expectedVoltageNeeded(rpm) + PIDController.calculate(e));
     }
+  }
+
+  private double expectedVoltageNeeded(double rpm) {
+    // 473rpm per V
+    return rpm / 473;
   }
 
   public void requestIdle() {
@@ -44,13 +56,23 @@ public class Shooter {
     requestShoot = true;
   }
 
+  private double getTime() {
+    return RobotController.getFPGATime()/1.0E3;
+  }
+
   public boolean atSetPoint() {
     return Math.abs(setpoint - getVelocity()) < 50;
   }
 
   public void periodic() {
     ShooterState nextSystemState = systemState;
-
+    if (requestShoot == false) {
+      systemState = ShooterState.IDLE;
+    } else {
+      if (systemState == ShooterState.IDLE) { 
+        systemState = ShooterState.APPROACHING_SETPOINT;
+      }
+    }
     if (systemState == ShooterState.IDLE) {
       setFlywheelRPM(0.0); // idle rpm
     } else if (! atSetPoint()) {
@@ -61,7 +83,7 @@ public class Shooter {
       setFlywheelRPM(setpoint);
     } else if (systemState == ShooterState.STABALIZING) {
       setFlywheelRPM(setpoint);
-      if (timenow() - timeLastStateChange >= 250) {
+      if (getTime() - timeLastStateChange >= 250) {
         systemState = ShooterState.AT_SETPOINT;
       }
     } else if (systemState == ShooterState.AT_SETPOINT) {
@@ -69,9 +91,11 @@ public class Shooter {
     }
 
     if (nextSystemState != systemState) {
-      timeLastStateChange = timenow();
+      timeLastStateChange = getTime();
     }
   }
+
+
 
   public enum ShooterState {
     IDLE, 
