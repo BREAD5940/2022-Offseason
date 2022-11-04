99cb0200c4a14aed7dae2d2a210653af8b36ffe8
EternalizedMoonlight <68178572+EternalizedMoonlight@users.noreply.github.com>
2022-11-04 13:45:14 -0700

    Renamed controller0 and controller1 to driver and operator for clarity

--- src/main/java/frc/robot/Controls.java
+++ src/main/java/frc/robot/Controls.java
@@ -10,22 +10,24 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.XboxController.Button;
 
 public class Controls {
-    public static XboxController controller0 = new XboxController(0);
-    public static XboxController controller1 = new XboxController(1);
+    public static XboxController driver = new XboxController(0);
+    public static XboxController operator = new XboxController(1);
     public Swerve swerve;
     public Shooter shooter;
     public Intake intake;
     public Gut gut;
     public Climber climber;
 
-    // get subsystems 
-    public Controls(Swerve swerve, Shooter shooter, Intake intake, Gut gut, Climber climber) {}
+    // get subsystems
+    public Controls(Swerve swerve, Shooter shooter, Intake intake, Gut gut, Climber climber) {
+    }
 
     public void periodic() {
+        /* Driver Controls */
         // Intake Controls
-        if (controller0.getRightTriggerAxis() >= 0.1) {
+        if (driver.getRightTriggerAxis() >= 0.1) {
             intake.requestDeploy(false);
-        } else if (controller0.getLeftTriggerAxis() >= 0.1) {
+        } else if (driver.getLeftTriggerAxis() >= 0.1) {
             intake.requestDeploy(true);
         } else {
             intake.requestStow();
@@ -34,25 +36,26 @@ public class Controls {
         // Swerve Controls
 
         // reset rotation
-        if (controller0.getRawButton(Button.kStart.value)) {
+        if (driver.getRawButton(Button.kStart.value)) {
             swerve.reset(new Pose2d());
         }
-        
-        // movement inputs
-        double x = controller0.getRightY();
-        double y = controller0.getRightX();
-        double omega = controller0.getLeftX();
 
-        // movement outputs 
+        // movement inputs
+        double x = driver.getRightY();
+        double y = driver.getRightX();
+        double omega = driver.getLeftX();
+
+        // movement outputs
         double dx = Math.abs(x) > 0.05 ? Math.pow(-x, 1) * Swerve.ROBOT_MAX_SPEED : 0.0;
         double dy = Math.abs(y) > 0.05 ? Math.pow(-y, 1) * Swerve.ROBOT_MAX_SPEED : 0.0;
         double rot = Math.abs(omega) > 0.1 ? Math.pow(-omega, 3) * 2.5 : 0.0;
         swerve.setSpeeds(dx, dy, rot);
 
+        /* Operator Controls */
         // Climber Controls
-        if (controller.getYButton()) {
+        if (operator.getYButton()) {
             climber.requestDeploy();
-        } else if (controller.getAButton()) {
+        } else if (operator.getAButton()) {
             climber.requestStow();
         } else {
             climber.requestStow();
